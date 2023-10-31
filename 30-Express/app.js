e55a7f97766b121373b5c7c6632adc138d7593ec
deknaa <kadekna20@gmail.com>
2023-10-31 10:43:34 +0800

    Updated Dependencies

--- 30-Express/app.js
+++ 30-Express/app.js
@@ -3,10 +3,25 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+let todos = [
+    {
+        id: 1, value: "Belajar Express",
+        id: 2, value: "Belajar ReactJS",
+        id: 3, value: "Belajar buat API"
+    }
+]
+
 app.get("/", (req, res) => {
     res.json({
         message: "Selamat datang di server express"
     });
+});
+
+app.get("/todos", (req, res) => {
+    res.status(200).json({
+        message: "Berhasil mendapatkan data todos",
+        data: todos
+    })
 });
 
 app.listen(PORT, () => {
